52655a14946304db8eb3aa5ca2a592c372e98231
Saqlaen <msaqlaenahmed98@gmail.com>
2022-12-11 14:15:34 +0530

    assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
+        public int product(int x, int y) { return x + y; }
+        public int product(int x, int y, int z) { return x + y + z; }
+        public double product(double x, double y) { return x + y; }
+    }
 
+    public static void main(String[] args ){
+        Product p  = new Product();
+        p.product(10, 20);
+        p.product(10, 20, 30 );
+        p.product(5.00, 7.00);
+    }
 }
